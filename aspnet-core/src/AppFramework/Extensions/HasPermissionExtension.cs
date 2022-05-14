4b1d43aa3490186d319a2c31d5eab025f08a33c0
henji <zhouhaogg789@outlook.com>
2022-05-14 13:58:09 +0800

    更新视图元素的权限绑定

--- aspnet-core/src/AppFramework/Extensions/HasPermissionExtension.cs
+++ aspnet-core/src/AppFramework/Extensions/HasPermissionExtension.cs
@@ -1,21 +1,25 @@
 ﻿using AppFramework.Common;
 using Prism.Ioc;
 using System;
+using System.Windows;
 using System.Windows.Markup;
 
 namespace AppFramework.Extensions
 {
     public class HasPermissionExtension : MarkupExtension
-    {
+    {   
         public string Text { get; set; }
 
         public override object ProvideValue(IServiceProvider serviceProvider)
         {
             if (string.IsNullOrWhiteSpace(Text))
-                return false;
+                return Visibility.Collapsed;
 
             var permissionService = ContainerLocator.Container.Resolve<IPermissionService>();
-            return permissionService.HasPermission(Text);
+            if (permissionService.HasPermission(Text))
+                return Visibility.Visible;
+
+            return Visibility.Collapsed;
         }
     }
 }
