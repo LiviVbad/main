595126efaae52c26d635eb33b0a2ceec483804d6
henji <zhouhaogg789@outlook.com>
2022-08-09 21:24:58 +0800

    更新弹出窗口的右上角关闭, 注释版本的验证接口属性

--- aspnet-core/src/AppFramework.Application.Shared/Editions/Dto/FeatureInputTypeDto.cs
+++ aspnet-core/src/AppFramework.Application.Shared/Editions/Dto/FeatureInputTypeDto.cs
@@ -2,15 +2,14 @@ using System.Collections.Generic;
 using Abp.Runtime.Validation;
 
 namespace AppFramework.Editions.Dto
-{
-	//Mapped in CustomDtoMapper
+{ 
 	public class FeatureInputTypeDto
 	{
 		public string Name { get; set; }
 
 		public IDictionary<string, object> Attributes { get; set; }
 
-		public IValueValidator Validator { get; set; }
+		//public IValueValidator Validator { get; set; }
 
 		public LocalizableComboboxItemSourceDto ItemSource { get; set; }
 	}
