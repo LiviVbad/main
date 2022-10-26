3767860d92d9be0eca6ed5cf5ea54151abda99a2
henjigg <zhouhaogg789@outlook.com>
2022-10-26 17:43:44 +0800

    添加聊天窗口

---
+++ aspnet-core/src/AppFramework.Admin/Views/Shared/FriendsChatView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls; 
+
+namespace AppFramework.Views
+{ 
+    public partial class FriendsChatView : UserControl
+    {
+        public FriendsChatView()
+        {
+            InitializeComponent();
+        }
+    }
+}
