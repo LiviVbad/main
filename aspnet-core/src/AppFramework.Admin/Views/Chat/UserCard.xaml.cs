35363afe97a774a7872ea6f4a860cdd7b996b02e
henjigg <zhouhaogg789@outlook.com>
2022-10-30 12:29:04 +0800

    更新用户的消息头

---
+++ aspnet-core/src/AppFramework.Admin/Views/Chat/UserCard.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace AppFramework.Views
+{
+    public partial class UserCard : UserControl
+    {
+        public UserCard()
+        {
+            InitializeComponent();
+        }
+    }
+}
