1a0f954b353cca046d6fd81651b775acc527b7c2
henji <zhouhaogg789@outlook.com>
2022-11-05 19:46:02 +0800

    [XF] 新增聊天界面的UI选择器及模板

--- aspnet-core/src/AppFramework.Mobile/ViewModels/Chat/FriendsChatViewModel.cs
+++ aspnet-core/src/AppFramework.Mobile/ViewModels/Chat/FriendsChatViewModel.cs
@@ -1,10 +1,225 @@
-﻿using System;
+﻿using Abp.Runtime.Security;
+using AppFramework.ApiClient;
+using AppFramework.Authorization.Users.Profile;
+using AppFramework.Chat;
+using AppFramework.Chat.Dto;
+using AppFramework.Shared.Models.Chat;
+using AppFramework.Shared.Services; 
+using Newtonsoft.Json;
+using Prism.Commands;
+using Prism.Events;
+using Prism.Navigation; 
 using System.Collections.Generic;
-using System.Text;
+using System.Collections.ObjectModel;
+using System.Linq; 
+using System.Threading.Tasks;
 
 namespace AppFramework.Shared.ViewModels
 {
     public class FriendsChatViewModel : NavigationViewModel
     {
+        public FriendsChatViewModel(IApplicationContext context,
+           IChatAppService chatApp,
+           IFriendChatService chatService,
+           IProfileAppService profileAppService,
+           IAccessTokenManager tokenManager,
+           IEventAggregator aggregator,
+           ProxyChatControllerService proxyChat)
+        {
+            this.context = context;
+            this.chatApp = chatApp;
+            this.chatService = chatService;
+            this.profileAppService = profileAppService;
+            this.tokenManager = tokenManager;
+            this.aggregator = aggregator;
+            this.proxyChat = proxyChat;
+
+            chatService.OnChatMessageHandler += ChatService_OnChatMessageHandler;
+            messages = new ObservableCollection<ChatMessageModel>();
+            SendCommand = new DelegateCommand(() => { });
+
+            PickFileCommand = new DelegateCommand(() => { });
+            PickImageCommand = new DelegateCommand(() => { });
+            OpenFolderCommand = new DelegateCommand<ChatMessageModel>(arg => { });
+        }
+
+        #region 字段/属性
+
+        private string userName;
+        private string message;
+        private FriendModel friend;
+        private readonly IApplicationContext context;
+        private readonly IChatAppService chatApp;
+        private readonly IFriendChatService chatService;
+        private readonly IProfileAppService profileAppService;
+        private readonly IAccessTokenManager tokenManager;
+        private readonly IEventAggregator aggregator;
+        private readonly ProxyChatControllerService proxyChat;
+        private ObservableCollection<ChatMessageModel> messages;
+
+        public string Message
+        {
+            get { return message; }
+            set { message = value; RaisePropertyChanged(); }
+        }
+
+        public FriendModel Friend
+        {
+            get { return friend; }
+            set { friend = value; RaisePropertyChanged(); }
+        }
+
+        public ObservableCollection<ChatMessageModel> Messages
+        {
+            get { return messages; }
+            set { messages = value; RaisePropertyChanged(); }
+        }
+
+        public DelegateCommand SendCommand { get; private set; }
+        public DelegateCommand PickImageCommand { get; private set; }
+        public DelegateCommand PickFileCommand { get; private set; }
+        public DelegateCommand<ChatMessageModel> OpenFolderCommand { get; private set; }
+
+        #endregion
+
+        #region 消息处理
+
+        /// <summary>
+        /// 加载用户的聊天记录
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        private async Task GetUserChatMessagesByUser(long userId)
+        {
+            await WebRequest.Execute(() =>
+                chatApp.GetUserChatMessages(new GetUserChatMessagesInput()
+                {
+                    UserId = userId
+                }), async result =>
+                {
+                    if (!result.Items.Any()) return;
+
+                    var list = Map<List<ChatMessageModel>>(result.Items);
+                    userName = chatService.Friends
+                    .FirstOrDefault(t => t.FriendUserId.Equals(list.First().TargetUserId)).FriendUserName;
+
+                    foreach (var item in list)
+                    {
+                        await UpdateMessageInfo(item);
+                        Messages.Add(item);
+                    }
+
+                    //aggregator.GetEvent<ScrollEvent>().Publish(true);
+                    await MarkAllUnreadMessages();
+                });
+        }
+
+        /// <summary>
+        /// 更新消息格式
+        /// </summary>
+        /// <param name="model"></param>
+        private async Task UpdateMessageInfo(ChatMessageModel model)
+        {
+            if (model.Side == ChatSide.Sender)
+            {
+                model.Color = "#009933";
+                model.UserName = context.LoginInfo.User.Name;
+            }
+            else
+                model.UserName = userName;
+
+            if (model.Message.StartsWith("[image]"))
+            {
+                model.MessageType = "image";
+
+                var accessToken = tokenManager.GetAccessToken();
+                var code = SimpleStringCipher.Instance.Encrypt(accessToken, AppConsts.DefaultPassPhrase);
+
+                var msg = model.Message.Replace("[image]", "");
+                var output = JsonConvert.DeserializeObject<ChatUploadFileOutput>(msg);
+                model.Message = output.Name; //显示文件名
+
+                var downloadUrl = ApiUrlConfig.DefaultHostUrl + $"Chat/GetUploadedObject?fileId={output.Id}" +
+                    $"&fileName={output.Name}" +
+                    $"&contentType={output.ContentType}" +
+                    $"&enc_auth_token={code}";
+
+                //await DownloadAsync(downloadUrl, AppConsts.DocumentPath, output.Name);
+            }
+            else if (model.Message.StartsWith("[file]"))
+            {
+                model.MessageType = "file";
+
+                var accessToken = tokenManager.GetAccessToken();
+                var code = SimpleStringCipher.Instance.Encrypt(accessToken, AppConsts.DefaultPassPhrase);
+
+                var msg = model.Message.Replace("[file]", "");
+                var output = JsonConvert.DeserializeObject<ChatUploadFileOutput>(msg);
+                model.Message = output.Name; //显示文件名
+
+                var downloadUrl = ApiUrlConfig.DefaultHostUrl + $"Chat/GetUploadedObject?fileId={output.Id}" +
+                   $"&fileName={output.Name}" +
+                   $"&contentType={output.ContentType}" +
+                   $"&enc_auth_token={code}";
+
+                //await DownloadAsync(downloadUrl, AppConsts.DocumentPath, output.Name);
+            }
+            else if (model.Message.StartsWith("[link]"))
+            {
+                model.MessageType = "link";
+                var msg = model.Message.Replace("[link]", "");
+                model.Message = JsonConvert.DeserializeObject<dynamic>(msg).message;
+            }
+            else
+            {
+                model.MessageType = "text";
+            }
+        }
+
+        /// <summary>
+        /// 接受消息
+        /// </summary>
+        /// <param name="chatMessage"></param>
+        private async void ChatService_OnChatMessageHandler(ChatMessageDto chatMessage)
+        {
+            var msg = Map<ChatMessageModel>(chatMessage);
+            await UpdateMessageInfo(msg);
+            Messages.Add(msg);
+
+            await MarkAllUnreadMessages();
+        }
+
+        /// <summary>
+        /// 标记消息已读
+        /// </summary>
+        /// <returns></returns>
+        private async Task MarkAllUnreadMessages()
+        {
+            await WebRequest.Execute(async () =>
+            {
+                await chatApp.MarkAllUnreadMessagesOfUserAsRead(new MarkAllUnreadMessagesOfUserAsReadInput()
+                {
+                    UserId = Friend.FriendUserId
+                });
+            });
+        }
+
+        #endregion
+
+        public override Task GoBackAsync()
+        {
+            chatService.OnChatMessageHandler -= ChatService_OnChatMessageHandler;
+            return base.GoBackAsync();
+        }
+
+        public override async void OnNavigatedTo(INavigationParameters parameters)
+        {
+            if (parameters.ContainsKey("Value"))
+            {
+                Friend = parameters.GetValue<FriendModel>("Value");
+
+                await GetUserChatMessagesByUser(Friend.FriendUserId);
+            }
+        }
     }
 }
