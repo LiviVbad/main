abf1b7f649ba582e076a97de9aef0d962ee07ae3
henjigg <zhouhaogg789@outlook.com>
2022-11-10 08:36:01 +0800

    [WPF] 优化命名和文件结构

--- aspnet-core/src/AppFramework.Admin/Services/Account/UserConfigurationManager.cs
+++ aspnet-core/src/AppFramework.Admin/Services/Account/UserConfigurationManager.cs
@@ -8,7 +8,7 @@ using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using Abp.Web.Models.AbpUserConfiguration;
-using AppFramework.Authorization.Users.Profile; 
+using AppFramework.Authorization.Users.Profile;
 using AppFramework.Shared.Services;
 using System.Diagnostics;
 
@@ -49,11 +49,11 @@ namespace AppFramework.Services.Account
 
             if (AccessTokenManager.IsUserLoggedIn)
             {
-                //Stopwatch stopWatch = new Stopwatch();
-                //stopWatch.Start();
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Start();
                 var chatService = ContainerLocator.Container.Resolve<IChatService>();
                 await chatService.ConnectAsync();
-                //stopWatch.Stop();
+                stopWatch.Stop();
             }
 
             var profileAppService = ContainerLocator.Container.Resolve<IProfileAppService>();
